c68c844e40b1271b56529f302372dee749affde2
Daisho Komiyama <daishokomiyama@gmail.com>
2019-03-06 08:47:00 -0500

    Adding init

--- my-carousel.js
+++ my-carousel.js
@@ -1,5 +1,25 @@
 const Caro = (function () {
+	const elem,
+		speed = .25;
+		duration = 2;
+		interactTimeout = 4,
+		id = null,
+		container = null,
+		slides = null,
+		prevBtn = null,
+		nextBtn = null,
+		index = 0,
+		count = null,
+		lock = false,
+		interval = null,
+		nav = null,
     function init (elementId) {
-
+    	elem = document.getElementById(elementId);
+    	console.log(elem);
     }
+    
+    return {
+    	init
+    };
+    
 }());
