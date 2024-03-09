3040c9846288640d2daad725dbacc6c9dd13ef30
Daisho Komiyama <daishokomiyama@gmail.com>
2024-03-09 13:39:50 -0500

    Add prettier

--- my-carousel.js
+++ my-carousel.js
@@ -1,288 +1,292 @@
 const Carousel = (function () {
-	const speed = .25;
-	const duration = 2;
-	const interactTimeout = 4;
-	let container = null;
-	let slides = null;
-	let prevBtn = null;
-	let nextBtn = null;
-	let index = 0;
-	let count = null;
-	let lock = false;
-	let interval = null;
-    let nav = null;
-    let delta;
-	let xStart;
-	let xLast;
-	let yStart;
-	let eStart;
-	let tLast;
-	let swipe;
-	let vel;
+    const speed = 0.25
+    const duration = 2
+    const interactTimeout = 4
+    let container = null
+    let slides = null
+    let prevBtn = null
+    let nextBtn = null
+    let index = 0
+    let count = null
+    let lock = false
+    let interval = null
+    let nav = null
+    let delta
+    let xStart
+    let xLast
+    let yStart
+    let eStart
+    let tLast
+    let swipe
+    let vel
 
-    function init (config) {
-    	carousel = document.getElementById(config.carousel);
-    	container = document.getElementById(config.container);
-		slides = document.getElementsByClassName(config.slide);
-    	prevBtn = document.getElementById(config.prev);
-    	nextBtn = document.getElementById(config.next);
-    	count = slides.length;
+    function init(config) {
+        carousel = document.getElementById(config.carousel)
+        container = document.getElementById(config.container)
+        slides = document.getElementsByClassName(config.slide)
+        prevBtn = document.getElementById(config.prev)
+        nextBtn = document.getElementById(config.next)
+        count = slides.length
 
-    	nextBtn && nextBtn.addEventListener('click', () => {
-    		slideLeft(index);
-    		resetTimer();
-    	}, false);
+        nextBtn &&
+            nextBtn.addEventListener(
+                'click',
+                () => {
+                    slideLeft(index)
+                    resetTimer()
+                },
+                false
+            )
 
-	    prevBtn && prevBtn.addEventListener('click', () => {
-	    	slideRight(index);
-	    	resetTimer();
-	    }, false);
+        prevBtn &&
+            prevBtn.addEventListener(
+                'click',
+                () => {
+                    slideRight(index)
+                    resetTimer()
+                },
+                false
+            )
 
-		container.addEventListener('touchstart', onTouchStart);
-		container.addEventListener('touchmove', onTouchMove);
-		container.addEventListener('touchend', onTouchEnd);
+        container.addEventListener('touchstart', onTouchStart)
+        container.addEventListener('touchmove', onTouchMove)
+        container.addEventListener('touchend', onTouchEnd)
 
-		addNav();
-		setTimer();
+        addNav()
+        setTimer()
     }
 
-	function slideLeft () {
-        cycle(1);
+    function slideLeft() {
+        cycle(1)
     }
 
-    function slideRight () {
-        cycle(-1);
+    function slideRight() {
+        cycle(-1)
     }
 
-    function incdec (inc) {
-        index = (index + count + inc) % count;
+    function incdec(inc) {
+        index = (index + count + inc) % count
     }
 
-    function recycle () {
-        let ind = (index + Math.floor(count / 2)) % count;
-        let pos = Math.floor(-count / 2);
+    function recycle() {
+        let ind = (index + Math.floor(count / 2)) % count
+        let pos = Math.floor(-count / 2)
         for (let i = 0; i < slides.length; i++) {
-            slides[ind].style.left = pos + '00%';
-            slides[ind].style.transition = 'left 0s';
-            pos++;
-            ind = (ind + 1) % count;
+            slides[ind].style.left = pos + '00%'
+            slides[ind].style.transition = 'left 0s'
+            pos++
+            ind = (ind + 1) % count
         }
     }
 
-    function cycle (dir) {
+    function cycle(dir) {
         if (lock || document.hidden) {
-            return;
+            return
         }
 
         lock = setTimeout(function () {
-        	const self = this;
-            if (lock !== true) lock = false;
-        }, speed * 900);
+            const self = this
+            if (lock !== true) lock = false
+        }, speed * 900)
 
         for (let i = 0; i < slides.length; i++) {
-            let pos = (i + (dir * count) - index) % count;
+            let pos = (i + dir * count - index) % count
             if (Math.abs(pos) <= 1) {
-                transition = 'left ' + speed + 's ease';
+                transition = 'left ' + speed + 's ease'
+            } else {
+                transition = 'left 0s'
             }
-            else {
-                transition = 'left 0s';
-            }
-            pos -= dir;
-            slides[i].style.left = pos + '00%';
-            slides[i].style.transition = transition;
+            pos -= dir
+            slides[i].style.left = pos + '00%'
+            slides[i].style.transition = transition
         }
-        incdec(dir);
-        index %= count;
-        updateNav();
+        incdec(dir)
+        index %= count
+        updateNav()
     }
 
-    function goto (localIndex) {
+    function goto(localIndex) {
         if (lock) {
-            clearTimeout(lock);
+            clearTimeout(lock)
             lock = setTimeout(function () {
-                lock = false;
-                goto(localIndex);
-            }, speed * 1000);
+                lock = false
+                goto(localIndex)
+            }, speed * 1000)
         }
-        let dest = [];
-        const gap = localIndex - index;
-        if (gap === 0) return;
-        const offset = gap > 0 ? count : -count;
+        let dest = []
+        const gap = localIndex - index
+        if (gap === 0) return
+        const offset = gap > 0 ? count : -count
         for (let i = 0; i < slides.length; i++) {
-            let pos = (i + offset - index) % count;
-            dest[i] = pos - gap;
-            let slide = slides[i];
-            slide.style.transition = 'left 0s';
-            slide.style.left = pos + '00%';
+            let pos = (i + offset - index) % count
+            dest[i] = pos - gap
+            let slide = slides[i]
+            slide.style.transition = 'left 0s'
+            slide.style.left = pos + '00%'
         }
 
         const move = function () {
             for (let i = 0; i < slides.length; i++) {
-                slides[i].style.left = dest[i] + '00%';
-                slides[i].style.transition = 'left ' + speed + 's ease';
+                slides[i].style.left = dest[i] + '00%'
+                slides[i].style.transition = 'left ' + speed + 's ease'
             }
-        };
+        }
 
-        (function () {
-        	//By using setTimeout, execuion can defer until thread is free
-        	setTimeout(move, 0);
-        }());
+        ;(function () {
+            // By using setTimeout, execution can defer until thread is free
+            setTimeout(move, 0)
+        })()
 
-        resetTimer();
+        resetTimer()
         lock = setTimeout(function () {
-            recycle();
-            lock = false;
-        }, speed * 1000);
-        index = localIndex;
-        updateNav();
+            recycle()
+            lock = false
+        }, speed * 1000)
+        index = localIndex
+        updateNav()
     }
 
-    function setTimer () {
+    function setTimer() {
         interval = setInterval(function () {
-            slideLeft();
-        }, duration * 1000);
+            slideLeft()
+        }, duration * 1000)
     }
 
-    function resetTimer () {
+    function resetTimer() {
         if (interval) {
-            clearInterval(interval);
-            interval = null;
+            clearInterval(interval)
+            interval = null
             setTimeout(function () {
-                setTimer();
-            }, interactTimeout * 1000);
+                setTimer()
+            }, interactTimeout * 1000)
         }
     }
 
-    function addNav () {
-        const navCnt = document.createElement('div');
-        navCnt.setAttribute('class', 'carousel-nav');
-        nav = [];
+    function addNav() {
+        const navCnt = document.createElement('div')
+        navCnt.setAttribute('class', 'carousel-nav')
+        nav = []
         for (let i = 0; i < count; i++) {
-            nav[i] = document.createElement('span');
-            let n = i;
+            nav[i] = document.createElement('span')
+            let n = i
             nav[i].addEventListener('click', () => {
-				goto(n);
-            });
-            navCnt.appendChild(nav[i]);
+                goto(n)
+            })
+            navCnt.appendChild(nav[i])
         }
-        carousel.appendChild(navCnt);
-        updateNav();
+        carousel.appendChild(navCnt)
+        updateNav()
     }
 
-    function updateNav () {
+    function updateNav() {
         for (let i = 0; i < count; i++) {
             if (i === index) {
-                nav[i].classList.add('active');
-            }
-            else {
-                nav[i].classList.remove('active');
+                nav[i].classList.add('active')
+            } else {
+                nav[i].classList.remove('active')
             }
         }
     }
 
-    function onTouchStart (event) {
-        if (swipe || lock === true) return;
-        lock = true;
-        xStart = xLast = event.changedTouches[0].pageX;
-        yStart = event.changedTouches[0].pageY;
-        eStart = parseInt(container.style.left) || 0;
-        tLast = new Date().valueOf();
-        vel = 0;
-        swipe = 'pending';
+    function onTouchStart(event) {
+        if (swipe || lock === true) return
+        lock = true
+        xStart = xLast = event.changedTouches[0].pageX
+        yStart = event.changedTouches[0].pageY
+        eStart = parseInt(container.style.left) || 0
+        tLast = new Date().valueOf()
+        vel = 0
+        swipe = 'pending'
     }
 
-    function onTouchMove (event) {
+    function onTouchMove(event) {
         if (!swipe || swipe === 'canceled' || swipe === 'coasting') {
-            return false;
+            return false
         }
-        let curr = event.changedTouches[0];
-        if (swipe ==='pending') {
+        let curr = event.changedTouches[0]
+        if (swipe === 'pending') {
             let dY = Math.abs(curr.pageY - yStart),
-                dX = Math.abs(curr.pageX - xStart);
+                dX = Math.abs(curr.pageX - xStart)
             if (dY > dX * 4 || dY > 24) {
-                swipe = 'canceled';
-                lock = false;
-                return;
-            }
-            else if (dX > dY * 3 || dX > 10) {
-                swipe = 'active';
-            }
-            else {
-                return;
+                swipe = 'canceled'
+                lock = false
+                return
+            } else if (dX > dY * 3 || dX > 10) {
+                swipe = 'active'
+            } else {
+                return
             }
         }
-        event.preventDefault();
+        event.preventDefault()
         let t = new Date().valueOf(),
             dT = t - tLast,
-            localVel = dT ? (curr.pageX - xLast) / dT : 0;
-        tLast = t;
-        vel = (2 * localVel + vel) / 3;
-        vel = Math.min(vel, 10);
-        xLast = curr.pageX;
-        delta = curr.pageX - xStart;
-        container.style.left = eStart + delta + 'px';
+            localVel = dT ? (curr.pageX - xLast) / dT : 0
+        tLast = t
+        vel = (2 * localVel + vel) / 3
+        vel = Math.min(vel, 10)
+        xLast = curr.pageX
+        delta = curr.pageX - xStart
+        container.style.left = eStart + delta + 'px'
     }
 
-    function onTouchEnd () {
+    function onTouchEnd() {
         if (swipe === 'coasting') {
-            return;
+            return
         }
         if (swipe === 'canceled') {
-            swipe = null;
-            return;
+            swipe = null
+            return
         }
         if (swipe === 'active') {
-            swipe = 'coasting';
-            setTimeout(inertia, 0);
+            swipe = 'coasting'
+            setTimeout(inertia, 0)
         }
     }
 
-    function inertia () {
+    function inertia() {
         let w = container.offsetWidth,
             gravity = 0,
             t = new Date().valueOf(),
-            dT = t - tLast;
-        tLast = t;
+            dT = t - tLast
+        tLast = t
         if (delta < -w / 6) {
-            gravity = -w;
+            gravity = -w
+        } else if (delta > w / 6) {
+            gravity = w
         }
-        else if (delta > w / 6) {
-            gravity = w;
-        }
-        let g = gravity - delta;
-        let dist = Math.abs(g);
-        let dir = dist ? g / dist : 0;
-        let accel;
+        let g = gravity - delta
+        let dist = Math.abs(g)
+        let dir = dist ? g / dist : 0
+        let accel
+
         if (false && dist > 20) {
-            accel = dT / (2 * dir * Math.sqrt(dist));
-            vel += accel;
-            vel *= .6;
+            accel = dT / (2 * dir * Math.sqrt(dist))
+            vel += accel
+            vel *= 0.6
+        } else {
+            vel = g / 60
         }
-        else {
-            vel = g / 60;
-        }
-        delta += vel * dT;
-        let pos = Math.floor(delta);
-        container.style.left = pos + 'px';
+        delta += vel * dT
+        let pos = Math.floor(delta)
+        container.style.left = pos + 'px'
+
         if (Math.abs(pos - gravity) > 2 || vel > 0.1) {
-            setTimeout(inertia, 30);
-        }
-        else {
+            setTimeout(inertia, 30)
+        } else {
             if (gravity) {
-                let dir = gravity > 0 ? -1 : 1;
-                incdec(dir);
-                updateNav();
-                recycle();
+                let dir = gravity > 0 ? -1 : 1
+                incdec(dir)
+                updateNav()
+                recycle()
             }
-            container.style.left = '0';
-            resetTimer();
-            swipe = null;
-            lock = false;
+            container.style.left = '0'
+            resetTimer()
+            swipe = null
+            lock = false
         }
     }
 
     return {
-    	init
-    };
-
-}());
+        init,
+    }
+})()
